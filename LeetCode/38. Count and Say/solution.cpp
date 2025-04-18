f2d39bbd63edec20c768c8c121580bbf1163cb48
Nikita Vasilev <nv3212@gmail.com>
2025-04-18 09:14:12 +0400

    LeetCode. 38. Count and Say

---
+++ LeetCode/38. Count and Say/solution.cpp
@@ -0,0 +1,44 @@
+/*
+
+38. Count and Say
+
+The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+
+countAndSay(1) = "1"
+countAndSay(n) is the run-length encoding of countAndSay(n - 1).
+Run-length encoding (RLE) is a string compression method that works by replacing consecutive identical characters (repeated 2 or more times) with the concatenation of the character and the number marking the count of the characters (length of the run). For example, to compress the string "3322251" we replace "33" with "23", replace "222" with "32", replace "5" with "15" and replace "1" with "11". Thus the compressed string becomes "23321511".
+
+Given a positive integer n, return the nth element of the count-and-say sequence.
+
+*/
+
+class Solution {
+    private:
+        std::string h(int n) {
+            if (n == 1) { return "1"; }
+    
+            std::string s = h(n - 1);
+            std::string res = "";
+    
+            int i = 0;
+    
+            while (i < s.size()) {
+                int cnt = 1;
+    
+                while (i + 1 < s.size() && s[i] == s[i + 1]) {
+                    cnt++;
+                    i++;
+                }
+    
+                res += std::to_string(cnt) + s[i];
+    
+                i++;
+            }
+    
+            return res;
+        }
+    public:
+        string countAndSay(int n) {
+            return h(n);
+        }
+    };
